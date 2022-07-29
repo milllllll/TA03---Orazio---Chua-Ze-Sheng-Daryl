39a9b7819f20b2c135e72fccb85c287ec56698f4
milllllll <109808452+milllllll@users.noreply.github.com>
2022-07-29 20:25:07 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -26,13 +26,13 @@ with profit_loss.open(mode="r",encoding="UTF-8", newline="") as info:
         #append days to list_day
         list_day.append((line[0]))
 
-    #create a For loop that repeat based on the number of sublist in the list_profit list (excluding the first sublist)
-    for i in range(1, len(list_profit)):
-        #create if and else statement
-        #set condition (net profit of a day is less than the net profit of the previous day) for if statement
-        if list_profit[i] < list_profit[i-1]:
-            #if condition is met, print the statement using print function and f-string
-            print(f"DAY: {list_day[i]}, AMOUNT: SGD{convertUSDtoSGD(list_profit[i])}")
+#create a For loop that repeat based on the number of sublist in the list_profit list (excluding the first sublist)
+for i in range(1, len(list_profit)):
+    #create if and else statement
+    #set condition (net profit of a day is less than the net profit of the previous day) for if statement
+    if list_profit[i] < list_profit[i-1]:
+    #if condition is met, print the statement using print function and f-string
+    print(f"DAY: {list_day[i]}, AMOUNT: SGD{convertUSDtoSGD(list_profit[i])}")
             #set the variable as False
             higher = False
         #set condition (net profit of a day is greater than the net profit of the previous day) for else statement
