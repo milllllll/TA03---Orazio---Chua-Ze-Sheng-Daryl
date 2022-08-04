44c3f3458d20ea2e83dd5b4febd1eebac24833f6
Janice-tan1 <109803271+Janice-tan1@users.noreply.github.com>
2022-08-04 14:20:19 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -21,6 +21,8 @@ def coh_function(forex):
     list_overall = []
     #open the Cash on Hand csv file in read mode using a with statement, to read its contents
     with coh.open(mode="r",encoding="UTF-8", newline="") as info:
+        #set the variable, 'higher' as True
+        higher = True
         #create a reader object
         reader = csv.reader(info)
         #create a for loop to retrieve the data from the reader object
@@ -36,45 +38,25 @@ def coh_function(forex):
             #if detected, it will return to the try statement and the continue keyword helps to repeat the code body again
             except ValueError:
                 continue
-
-
-#create a file path using Path.cwd() and assign it to a variable, coh_path
-coh_path = Path.cwd()/"csv_reports"/"cash-on-hand.csv"
-
-#create and empty list and assign to a varibale called list_coh
-list_coh = []
-#create and empty list and assign it to a variable called list_day
-list_day = []
-#create a variable name called higher meaning that as the coh each day increased is True
-higher = True
-
-#use with keyword to read the coh_path
-with coh_path.open(mode="r",encoding="UTF-8", newline="") as info:
-    #assign variable called reader to read the csv file 
-    reader = csv.reader(info)
-    #use next to remove the header from the csv file
-    next(reader)
-    #create a for loop and assign a variable name for the data in the reader
-    for line in reader:
-        #append the coh amount for each day to the empty list called list_coh
-        list_coh.append(float(line[1]))
-        #append the days to the empty list called list_day
-        list_day.append(line[0])
-
-    #create a for loop and assign a variable name in the data range of 1 to the number of datas in the list called list_coh
-    for i in range(1, len(list_coh)):
-        #use if keyword for when the amount in the list_coh decreases from the previous entry
-        if list_coh[i] < list_coh[i-1]:
-            #print the day the amount decreases from the previous entry and the amount using the conversion rate imported
-            print(f"DAY: {list_day[i]}, AMOUNT: SGD{convertUSDtoSGD(list_coh[i])}")
-            #assign a variable that higher is False
-            higher = False
-        #use else keyword to continue to the next iteration if the above is False
-        else:
-            #use continue keyword to move to the next iteration 
-            continue
-
-    #use if keyword and assign if higher equates to True
-    if higher == True:
-        #print the output saying CASH ON EACH DAY IS HIGHER THEN THE PREVIOUS DAY
-        print(f"CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
+        #create a for loop that repeats the code body based on the number of sublists in list_coh 
+        #this is excluding the first sublist, since there is no previous day data to compare with
+        for i in range(1, len(list_coh)):
+            #create if and else statements
+            #set condition (coh is lower than the previous day) for the if statement
+            if list_coh[i] < list_coh[i-1]:
+                #if condition is met, set variable, 'higher' as False
+                higher = False
+                #append the following f-string into the empty list, 'list_overall' 
+                #use the (value difference * forex) equation to convert USD to SGD, and round off the value to 2 decimal place using round() function
+                list_overall.append(f"[CASH DEFICIT] DAY: {list_day[i]}, AMOUNT: SGD{round((list_coh[i-1]-list_coh[i])*forex,2)}\n")
+            #set condition (coh is higher than the previous day) for the else statement
+            #if the condition of the if statement is not met, the code body under the else keyword will be ran
+            else:
+                #use continue keyword to move to the next iteration
+                continue
+        #create another if statement and set the condition, higher == True
+        if higher == True:
+            #if the condition is met, append the following f-string to the empty list, list_overall, using append() function
+            list_overall.append(f"[CASH SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY\n")
+        #return the 'list_overall' using return keyword
+        return list_overall
