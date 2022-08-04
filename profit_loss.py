44eeaa15689f23f96e723da02459ad7b24654f18
Xinleeee <109804291+Xinleeee@users.noreply.github.com>
2022-08-04 14:49:29 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -1,25 +1,43 @@
-#import Path
+#imort Path from the built-in module, pathlib
 from pathlib import Path
-#import csv
+#import the built-in module, csv
 import csv
-#import convertUSDtoSGD from api file
-from api import convertUSDtoSGD
-#create path for profit_loss
-profit_loss = Path.cwd()/"csv_reports"/"profit-and-loss.csv"
 
-#create empty list for profit
-list_profit = []
-#create empty list for day
-list_day = []
-#set variable as True
-higher = True
-#open profit-and-loss.csv in read mode using with statement
-with profit_loss.open(mode="r",encoding="UTF-8", newline="") as info:
-    #use csv.reader() to read information in csv file and assign to variable, reader
-    reader = csv.reader(info)
-    #skip headers in reader
-    next(reader)
-    #create For loop 
+#create a function, profitloss_function(), with the parameter, forex
+def profitloss_function(forex):
+    #provide a description of the function, profitloss_function(forex) using docstring
+    """
+    The program computes the difference in the net profit between each day and returns
+    the days where net profit is lower than the previous day and the value difference.
+    One parameter is required: forex, which will be the value of SGD for each dollar of USD.
+    """
+    #create a file path using Path.cwd() to locate the csv file and assign it to a variable, profit_loss
+    profit_loss = Path.cwd()/"csv_reports"/"Profits and Loss.csv"
+    #create and empty list and assign to a variable called list_profit
+    list_profit = []
+    #create and empty list and assign it to a variable called list_day
+    list_day = []
+    #create and empty list and assign it to a variable called list_overall
+    list_overall = []
+    #open the Profits and Loss csv file in read mode using a with statement, to read its contents
+    with profit_loss.open(mode="r",encoding="UTF-8", newline="") as info:
+        #create a reader object
+        reader = csv.reader(info)
+        #create a for loop to retrieve the data from the reader object
+        for line in reader:
+            #use try to start the exception handling
+            try:
+                #append the cash on hand value for each day into the empty list, list_profit, using the append() function
+                #use the float() function to convert the net profit value from a string to a float
+                list_profit.append(float(line[4]))
+                #append the day number into the empty list, list_day, using the append() function
+                #use the int() function to convert the day from a string to a integer
+                list_day.append(int(line[0]))
+            #use except to prevent 'ValueError' from crashing the program, since the headers are strings of words that cannot be converted into a float
+            #if detected, it will return to the try statement and the continue keyword helps to repeat the code body again
+            except ValueError:
+                continue
+
     for line in reader:
         #append net profits to empty list (list_profit) using append and indexing function
         list_profit.append(int(line[4]))
