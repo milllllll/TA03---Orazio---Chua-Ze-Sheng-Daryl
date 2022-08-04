c2e8f8e8bf76bdefe82741649ff280957142eb21
esHaA22 <109810218+esHaA22@users.noreply.github.com>
2022-08-04 14:03:12 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,24 +1,23 @@
-# import requests
-import requests
-# assign url as a variable and paste the link from alphavantage under Exchange Currency
-url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=W677J7965EU3DEBN"
-# request access to the data using .get() method and assign a variable called "response" to it
-response = requests.get(url)
-# retrieve data using .json() and save it as variable (detail)
-details = response.json()
-# create For loop 
-for info in details:
-    # create a variable ("conversion rate") and assign it to convert from USD to SGD using f-string
-    conversion_rate = f"USD1 = SGD{details[info]['5. Exchange Rate']}"
-# create a function (convertUSDtoSGD) with a parameter, amount
-def convertUSDtoSGD(amount):
-    # create For loop 
+# create a function, 'convertUSDtoSGD' 
+def convertUSDtoSGD():
+    # give a description of the function using docstrings
+    """
+    The program extracts from alphavantage.co and returns 
+    the real time currency conversion rate. 
+    No parameter is required.
+    """
+    #import built-in function, 'requests'
+    import requests
+    #create variable, 'url' to locate Exchange Currency from alphavantage.co
+    url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=W677J7965EU3DEBN"
+    #create variable, 'response' to request using requests keyword and access the data using .get() method
+    response = requests.get(url)
+    #create variable, 'details' to retrieve data using .json()
+    details = response.json()
+    #create For loop 
     for info in details:
-        # create a variable (USD) and assign it as the conversion rate (USD to SGD)
-        # convert from string to float 
+        #create variable, 'USD' and assign it as the conversion rate (USD to SGD)
+        #convert from string to float using float() function
         USD = float(details[info]['5. Exchange Rate'])
-    # return the amount in SGD 
-    return amount*USD
-# print "conversion rate"
-print(conversion_rate)
-
+    #return USD using return keyword
+    return USD
